e963014fe53ecfdd04fac38a161bc2d34567efbc
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2025-04-25 13:53:31 -0300

    feat: Implement FrameComponent with modal functionality and tab navigation in GerenciamentoComponent

--- front-angular/src/app/home/gerenciamento/gerenciamento.component.ts
+++ front-angular/src/app/home/gerenciamento/gerenciamento.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
+import { FrameComponent } from '../../components/frame/frame.component';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-gerenciamento',
-  template: `<div>Gerenciamento</div>`
+  templateUrl: './gerenciamento.component.html', 
+  imports: [FrameComponent,NgIf],  
 })
-export class GerenciamentoComponent {}
+export class GerenciamentoComponent {
+  showModal = false;
+  tab: string = 'autores';
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+}
